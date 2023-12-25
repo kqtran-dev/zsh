dc725224b46cb9ba7202ec616e3adb4e2c68350a
Kevin Q. Tran <kevin.tran@parker.com>
2023-12-24 17:10:45 -0800

    compatibility

--- .zim/init.zsh
+++ .zim/init.zsh
@@ -1,10 +1,10 @@
-zimfw() { source /Users/k/.config/zsh/.zim/zimfw.zsh "${@}" }
-zmodule() { source /Users/k/.config/zsh/.zim/zimfw.zsh "${@}" }
-fpath=(/Users/k/.config/zsh/.zim/modules/zsh-completions/src /Users/k/.config/zsh/.zim/modules/prompt-pwd/functions /Users/k/.config/zsh/.zim/modules/git-info/functions ${fpath})
+zimfw() { source /home/k/.config/zsh/.zim/zimfw.zsh "${@}" }
+zmodule() { source /home/k/.config/zsh/.zim/zimfw.zsh "${@}" }
+fpath=(/home/k/.config/zsh/.zim/modules/zsh-completions/src /home/k/.config/zsh/.zim/modules/prompt-pwd/functions /home/k/.config/zsh/.zim/modules/git-info/functions ${fpath})
 autoload -Uz -- prompt-pwd coalesce git-action git-info
-source /Users/k/.config/zsh/.zim/modules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source /Users/k/.config/zsh/.zim/modules/zsh-autosuggestions/zsh-autosuggestions.zsh
-source /Users/k/.config/zsh/.zim/modules/completion/init.zsh
-source /Users/k/.config/zsh/.zim/modules/prompt/init.zsh
-source /Users/k/.config/zsh/.zim/modules/asciiship/asciiship.zsh-theme
-source /Users/k/.config/zsh/.zim/modules/eriner/eriner.zsh-theme
+source /home/k/.config/zsh/.zim/modules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /home/k/.config/zsh/.zim/modules/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /home/k/.config/zsh/.zim/modules/completion/init.zsh
+source /home/k/.config/zsh/.zim/modules/prompt/init.zsh
+source /home/k/.config/zsh/.zim/modules/asciiship/asciiship.zsh-theme
+source /home/k/.config/zsh/.zim/modules/eriner/eriner.zsh-theme
