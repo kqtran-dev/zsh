f199583454239e6a6a47f4163c6f8cb2376c1a58
Kevin Q. Tran <kevin.tran@parker.com>
2023-12-24 18:02:29 -0800

    lang tab changes

--- .zim/init.zsh
+++ .zim/init.zsh
@@ -4,7 +4,5 @@ fpath=(/home/k/.config/zsh/.zim/modules/zsh-completions/src /home/k/.config/zsh/
 autoload -Uz -- prompt-pwd coalesce git-action git-info
 source /home/k/.config/zsh/.zim/modules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source /home/k/.config/zsh/.zim/modules/zsh-autosuggestions/zsh-autosuggestions.zsh
-source /home/k/.config/zsh/.zim/modules/completion/init.zsh
 source /home/k/.config/zsh/.zim/modules/prompt/init.zsh
-source /home/k/.config/zsh/.zim/modules/asciiship/asciiship.zsh-theme
 source /home/k/.config/zsh/.zim/modules/eriner/eriner.zsh-theme
--- .zprofile
+++ .zprofile
@@ -12,14 +12,13 @@ if [[ "$OSTYPE" == darwin* ]]; then
     path+='/opt/homebrew/bin'
 fi 
 
-export LC_ALL=C # changes for x3270
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8 # changes for x3270
+# export LC_ALL=C # changes for x3270
 
 export EDITOR='nvim'
 export VISUAL='nvim'
 
-if [[ -z "$LANG" ]]; then
-  export LANG='en_US.UTF-8'
-fi
 
 # Ensure path arrays do not contain duplicates
 typeset -gU cdpath fpath mailpath path
